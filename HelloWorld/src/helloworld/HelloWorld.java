b612c4e04d7d2117d9afb6db55f288fefa6bb819
Eric Charnesky <eric.charnesky@careevolution.com>
2022-01-12 10:28:11 -0500

    Hello World

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+
+package helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello world!");
+    }
+    
+}
